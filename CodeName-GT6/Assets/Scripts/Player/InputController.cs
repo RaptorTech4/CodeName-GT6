89ed2fb789c1b106df039c1e7d1e59f8d9b089c0
RaptorTech4 <33655338+RaptorTech4@users.noreply.github.com>
2018-08-06 21:55:08 +0200

    playerUpdate

---
+++ CodeName-GT6/Assets/Scripts/Player/InputController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    public float Vertical;
+    public float Horizontal;
+    public Vector2 MouseInput;
+
+    public void Update()
+    {
+        Vertical = Input.GetAxis("Vertical");
+        Horizontal = Input.GetAxis("Horizontal");
+        MouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    }
+
+}
