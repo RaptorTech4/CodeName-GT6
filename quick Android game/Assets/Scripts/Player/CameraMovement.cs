7cee8638a0d5d5e7ca83bebab0e373b6719cb241
Julez137 <36993671+Julez137@users.noreply.github.com>
2018-09-17 06:35:49 +0200

    new small game

---
+++ quick Android game/Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 cameraposition = new Vector3(player.transform.position.x,gameObject.transform.position.y,-10.0f);
+		transform.position = cameraposition;
+	}
+}
---
+++ quick Android game/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    #region Variables
+
+    [SerializeField] float speed, jumppower;
+    [SerializeField] bool IsGrounded;
+    private Rigidbody2D RB;
+
+
+    #endregion Variables
+
+
+    
+    void Start () 
+    {
+		RB = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        
+        RB.velocity = new Vector2( speed , RB.velocity.y);
+
+		if(Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+        {
+            RB.velocity = Vector2.up * jumppower;
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.transform.tag == "floor")
+        {
+            IsGrounded = true;
+        }
+    }
+
+     void OnCollisionExit2D(Collision2D collisionInfo)
+    {
+        if(collisionInfo.transform.tag == "floor")
+        {
+            IsGrounded = false;
+        }
+    }
+
+}
